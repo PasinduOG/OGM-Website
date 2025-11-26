add5475f7418b39ec99c445fa4b5394bafa481af
Pasindu Madhuwantha <126347762+PasinduOG@users.noreply.github.com>
2025-11-26 09:21:43 +0530

    enhancement: Refactor search functionality and improve UI elements for better user experience

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,18 @@
 let dataContainer = document.getElementById("dataContainer");
 let listHeader = document.getElementById("listHeader");
-let search = document.getElementById("txtSearch").value;
 let resultContainer = document.getElementById("resultContainer");
+let searchSection = document.getElementById("searchSection");
 
 const defaultImage = 'assets/img/default.jpg';
 const API_KEY = "f60eb7d2";
 
 function getAllResults(type) {
-    resultContainer.innerHTML = "";
-    dataContainer.innerHTML = "";
+
+    if (resultContainer != null && dataContainer != null) {
+        resultContainer.innerHTML = "";
+        dataContainer.innerHTML = "";
+        searchSection.classList.remove("d-none");
+    }
 
     if (type == "movie") {
         listHeader.innerText = "Latest Movies"
@@ -40,7 +44,7 @@ function getAllResults(type) {
                                         <span class="badge bg-danger">2025</span>
                                     </div>
                                     <div class="d-flex justify-content-end align-items-center">
-                                        <button class="btn btn-sm btn-secondary">View Details</button>
+                                        <button class="btn btn-sm btn-secondary" onclick="viewContent('${results[i].imdbID}')">View Details</button>
                                     </div>
                                 </div>
                             </div>
@@ -93,7 +97,7 @@ function getSuggestResults(keyword, type) {
                                     <span class="badge bg-secondary">${movie.Type}</span>
                                 </div>
                                 <div class="d-flex justify-content-end align-items-center">
-                                    <button class="btn btn-sm btn-secondary">View Details</button>
+                                    <button class="btn btn-sm btn-secondary" onclick="viewContent('${movie.imdbID}')">View Details</button>
                                 </div>
                             </div>
                         </div>
@@ -110,10 +114,8 @@ function getSuggestResults(keyword, type) {
 }
 
 function searchMovie() {
-
     dataContainer.innerHTML = "";
-
-    let search = document.getElementById("txtSearch").value.trim();
+    let search = document.getElementById("txtSearch").value;
 
     if (search === "") {
         getAllResults("movie");
@@ -125,23 +127,27 @@ function searchMovie() {
         .then(result => {
 
             if (result.Response === "False") {
-                resultContainer.classList.remove("d-none");
-                resultContainer.innerHTML = `
-                    <div class="alert alert-danger">No results found.</div>
-                `;
-                getSuggestResults('movie');
+                Swal.fire({
+                    title: "OOPS...",
+                    text: "No results found!",
+                    icon: "error"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.reload();
+                    }
+                });
                 return;
             }
 
             resultContainer.classList.remove("d-none");
 
             resultContainer.innerHTML = `
-            <div class="card bg-dark text-white py-5 shadow-lg">
+                <div class="card bg-dark text-white py-5 shadow-lg">
                     <div class="container py-4">
                         <div class="row align-items-center">
-                            <div class="col-lg-3 col-md-4 mb-4 mb-md-0">
-                                <img src="${result.Poster!= "N/A" ? result.Poster : defaultImage}"
-                                    alt="Movie Poster" class="img-fluid rounded shadow-lg">
+                            <div class="col-lg-3 col-md-4 mb-4 mb-md-0 text-center text-md-start">
+                                <img src="${result.Poster != "N/A" ? result.Poster : defaultImage}"
+                                    alt="Movie Poster" class="img-fluid rounded shadow-lg d-inline-block">
                             </div>
                             <div class="col-lg-9 col-md-8">
                                 <div class="d-flex align-items-center mb-3 flex-wrap">
@@ -154,14 +160,70 @@ function searchMovie() {
                                         class="text-danger fw-semibold">${result.Genre}</span>
                                 </p>
                                 <p class="mb-4">${result.Plot}</p>
-                                <button class="btn btn-danger px-4">View more</button>
+                                <a class="btn btn-danger px-4" onclick="viewContent('${result.imdbID}')">View more</a>
                             </div>
                         </div>
                     </div>
                 </div>
         `;
             let genre = result.Genre.split(",");
-            console.log(genre[0]);
             getSuggestResults(genre[0], result.Type);
+        });
+}
+
+function viewContent(id) {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`)
+        .then(res => res.json())
+        .then(data => {
+
+            searchSection.classList.add("d-none");
+
+            resultContainer.innerHTML = `
+            <div class="card bg-dark text-light border-0 rounded-3 shadow-lg mt-4">
+                <div class="card-body p-4">
+                    <div class="row g-4">
+                        <!-- Movie Poster -->
+                        <div class="col-lg-3 col-md-4">
+                            <img src="${data.Poster != "N/A" ? data.Poster : defaultImage}"
+                                alt="Movie Poster" class="img-fluid rounded shadow">
+                        </div>
+
+                        <!-- Movie Details -->
+                        <div class="col-lg-9 col-md-8">
+                            <h1 class="display-5 fw-bold mb-3">${data.Title}</h1>
+                            <div class="mb-3">
+                                <p class="mb-1"><strong>Release:</strong> ${data.Year}</p>
+                                <p class="mb-1"><strong>Category:</strong> ${data.Genre}</p>
+                                <p class="mb-1"><strong>Runtime:</strong> ${data.Runtime}</p>
+                            </div>
+
+                            <div class="mb-4">
+                                <p class="lead">${data.Plot}</p>
+                            </div>
+
+                            <div class="mb-3">
+                                <p class="mb-1"><strong>Writer:</strong> ${data.Writer}</p>
+                                <p class="mb-1"><strong>Starring:</strong> ${data.Actors}</p>
+                                <p class="mb-1"><strong>Language:</strong> ${data.Language}</p>
+                                <p class="mb-1"><strong>Country:</strong> ${data.Country}</p>
+                                <p class="mb-1"><strong>Awards:</strong> ${data.Awards}</p>
+                            </div>
+
+                            <div class="mb-4">
+                                <p class="mb-2"><strong>IMDb Ratings:</strong> <span class="text-warning"><i class="bi bi-star-fill text-warning"></i></span>
+                                ${data.imdbRating}</p>
+                            </div>
+
+                            <button class="btn btn-danger btn-lg px-5" onclick="window.location.reload()">Go back to home</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+            let genre = data.Genre.split(",");
+            getSuggestResults(genre[0], result.Type);
+
+
         });
 }
